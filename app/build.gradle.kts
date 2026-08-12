679355e95b6edcfbea3d15ce7a682b361b2dc906
sakulik <suxvshen@gmail.com>
2026-08-13 04:47:28 +0800

    fix(test): isolate debug application data

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,6 +25,10 @@ android {
     }
 
     buildTypes {
+        debug {
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-debug"
+        }
         release {
             isMinifyEnabled = true
             isShrinkResources = true
